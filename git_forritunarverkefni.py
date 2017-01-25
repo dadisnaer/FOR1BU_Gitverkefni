832f12485eeb49e1e5182b254886498fd4a65fd2
dadisnaer <dadish2230@gmail.com>
2017-01-25 15:10:51 +0000

    Dæmi 1 búið

---
+++ git_forritunarverkefni.py
@@ -0,0 +1,17 @@
+#Daði Snær Hálfdánsson
+#25.01.17
+#Git verkefni
+
+#Liður 1
+#Bið notanda um að slá inn tölu eitt
+tala1=int(input("Sláðu inn tölu eitt"))
+#Bið notanda um að slá inn tölu tvö
+tala2=int(input("Sláðu inn tölu tvö"))
+#Plúsa tölurnar saman
+plus=tala1+tala2
+#Margfalda tölurnar saman
+marg=tala1*tala2
+#Birti tölurnar lagðar saman
+print("Tölurnar lagðar saman:",plus)
+#Birti tölurnar margfaldaðar saman
+print("Tölurnar margfaldaðar saman:",marg)
